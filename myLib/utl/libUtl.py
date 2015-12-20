ef76a1a97eb20e4e26fc088edcefc590ae6db105
dmalnati <douglas.malnati@gmail.com>
2015-12-20 11:06:20 -0500

    Pi servo class now stops applying PWM after a delay, now quieter operation

--- myLib/utl/libUtl.py
+++ myLib/utl/libUtl.py
@@ -106,7 +106,11 @@ def GetPrettyJSON(jsonObj):
 
 
 
-
+#http://code.activestate.com/recipes/52308-the-simple-but-handy-collector-of-a-bunch-of-named/?in=user-97991
+class Bunch(dict):
+    def __init__(self, **kwds):
+        dict.__init__(self, kwds)
+        self.__dict__.update(kwds)
 
 
 
