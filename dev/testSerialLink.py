4b497aebf2b34e98431b3ec3b899e266dc4f493c
dmalnati <douglas.malnati@gmail.com>
2016-03-22 00:07:56 -0400

    Pi SerialLink mid-testing checkin

--- dev/testSerialLink.py
+++ dev/testSerialLink.py
@@ -20,18 +20,21 @@ class CRC8:
     def __init__(self):
         pass
 
-    def Calculate(self, buf):
-        return self.calcCheckSum(buf)
+    @staticmethod
+    def Calculate(buf):
+        return CRC8.calcCheckSum(buf)
 
-    def calcCheckSum(self, incoming):
+    @staticmethod
+    def calcCheckSum(incoming):
         #msgByte = hexStr2Byte(incoming)
         msgByte = bytearray(incoming)
         check = 0
         for i in msgByte:
-            check = self.AddToCRC(i, check)
+            check = CRC8.AddToCRC(i, check)
         return check
 
-    def AddToCRC(self, b, crc):
+    @staticmethod
+    def AddToCRC(b, crc):
         b2 = b
         if (b < 0):
             b2 = b + 256
@@ -47,16 +50,55 @@ class CRC8:
 
 
 
+class SerialLinkHeader():
+    def __init__(self, data, reserve = False):
+        self.data = data
+
+        if reserve:
+            self.data.extend([0] * 4)
+
+    @staticmethod
+    def GetSize():
+        return 4
+
+    def SetPreamble(self, preamble):
+        self.data[0] = preamble
+
+    def GetPreamble(self):
+        return self.data[0]
+
+    def SetDataLength(self, dataLength):
+        self.data[1] = dataLength
+
+    def GetDataLength(self):
+        return self.data[1]
+
+    def SetChecksum(self, checksum):
+        self.data[2] = checksum
+
+    def GetChecksum(self):
+        return self.data[2]
+
+    def SetProtocolId(self, protocolId):
+        self.data[3] = protocolId
+
+    def GetProtocolId(self):
+        return self.data[3]
+
 
 class SerialLink():
     def __init__(self):
         self.BAUD              = 9600
+        self.PREAMBLE_BYTE     = 0x55
         self.RX_POLL_PERIOD_MS = 100
 
         self.bcPinRx = None
         self.bcPinTx = None
 
         self.cbOnRxAvailable = None
+
+        self.state = "LOOKING_FOR_PREAMBLE_BYTE"
+        self.bufRx = bytearray()
 
         self.pigd = pigpio.pi()
 
@@ -71,81 +113,169 @@ class SerialLink():
 
     def InitRx(self):
         # open pin for reading serial
+        pigpio.exceptions = False
+        self.pigd.bb_serial_read_close(self.bcPinRx)
+        pigpio.exceptions = True
         self.pigd.bb_serial_read_open(self.bcPinRx, self.BAUD)
 
         # set up timer to check on incoming data
         evm_SetTimeout(self.OnTimeoutCheckRx, self.RX_POLL_PERIOD_MS)
 
     def InitTx(self):
+        pigpio.exceptions = False
         self.pigd.set_mode(self.bcPinTx, pigpio.OUTPUT)
+        pigpio.exceptions = True
 
-        # set up timer to periodically send data
-        evm_SetTimeout(self.OnTimeoutTx, 2000)
-
-
-
-
-    def Send(self, buf):
-        Log("Sending")
-        preamble = 0x55
-
-        # create frame
+    def Send(self, protocolId, buf):
+        # Create frame
         msg = bytearray() 
 
-        msg.append(preamble)    # preamble
-        msg.append(len(buf))    # dataLength
-        msg.append(0x00)        # checksum
-        msg.append(0x01)        # protocolId
-        #msg.append(buf)         # data
-        msg.extend(buf)         # data
+        # Fill out header
+        hdr = SerialLinkHeader(msg, reserve = True)
+        hdr.SetPreamble(self.PREAMBLE_BYTE)
+        hdr.SetDataLength(len(buf))
+        hdr.SetChecksum(0x00)
+        hdr.SetProtocolId(protocolId)
 
-        crc = CRC8()
-        checksum = crc.Calculate(msg)
+        # Add data
+        msg.extend(buf)
 
-        msg[2] = checksum
+        # Calculate Checksum
+        checksum = CRC8.Calculate(msg)
 
-        # create serial waveform
+        # Update the message checksum now that it is calculated
+        hdr.SetChecksum(checksum)
+
+        # Create serial waveform
         self.pigd.wave_clear()
         self.pigd.wave_add_serial(self.bcPinTx, self.BAUD, msg)
 
         serialWave = self.pigd.wave_create()
 
-        # send serial data
+        # Send serial data
         self.pigd.wave_send_once(serialWave)
         self.pigd.wave_delete(serialWave)
 
-        # wait for TX to complete
+        # Wait for TX to complete
         while self.pigd.wave_tx_busy():
             pass
 
-        for byte in msg:
-            v = float(byte) / 255. * 5.0
-
-            Log(("%3i" % byte) + " -> " + ("%.2f" % v))
-        
-
-        Log("Sending Complete")
-
-
-
-    def OnTimeoutCheckRx(self):
+    def TryToSyncStreamFromSerial(self):
         (count, byteList) = self.pigd.bb_serial_read(self.bcPinRx)
 
-        if count:
-            print("Got " + str(count) + " bytes:")
+        for byte in byteList:
+            if self.state == "LOOKING_FOR_PREAMBLE_BYTE":
+                if byte == 0x55:
+                    self.bufRx.append(byte)
+                    self.state = "LOOKING_FOR_END_OF_MESSAGE"
+            elif self.state == "LOOKING_FOR_END_OF_MESSAGE":
+                self.bufRx.append(byte)
 
-            for byte in byteList:
-                v = float(byte) / 255. * 5.0
+    def TryToAddMoreDataFromSerial(self):
+        # due to unfortunate way of reading from serial, and the way the
+        # sync function is then implemented, calling the sync function is
+        # what is needed here
+        self.TryToSyncStreamFromSerial()
 
-                Log(("%3i" % byte) + " -> " + ("%.2f" % v))
+    def ClearLeadingBufferBytesAndRecalibrate(self, removeLen):
+        bufRxSize = len(self.bufRx)
+
+        if removeLen < bufRxSize:
+            del self.bufRx[0:removeLen]
+
+            # Search for preamble byte
+            try:
+                idx = self.bufRx.index(bytes(self.PREAMBLE_BYTE))
+
+                # Shift all bytes to start of buffer
+                del self.bufRx[0:idx]
+                self.state = "LOOKING_FOR_END_OF_MESSAGE"
+            except ValueError:
+                # Couldn't find preamble
+                bufRxSizeNew = len(self.bufRx)
+
+                del self.bufRx[0:bufRxSizeNew]
+                self.state = "LOOKING_FOR_PREAMBLE_BYTE"
+        else:
+            # Should never happen.  Zero out the buffer.
+            del self.bufRx[0:bufRxSize]
+            self.state = "LOOKING_FOR_PREAMBLE_BYTE"
+
+    def TryToProcessMessageFromCachedData(self):
+        bufRxSize = len(self.bufRx)
+
+        if bufRxSize >= (SerialLinkHeader.GetSize()):
+            hdr = SerialLinkHeader(self.bufRx)
+
+            msgSize = SerialLinkHeader.GetSize() + hdr.GetDataLength()
+
+            if bufRxSize >= msgSize:
+                # keep a cached copy of the checksum since the current value
+                # will be overwritten during validation
+                checksumTmp = hdr.GetChecksum()
+
+                # zero out message checksum for local calculation
+                hdr.SetChecksum(0x00)
+
+                # Calculate Checksum
+                #crc = CRC8()
+                checksum = CRC8.Calculate(self.bufRx[0:msgSize])
+
+                # Restore the message checksum
+                hdr.SetChecksum(checksumTmp)
+
+                # Validate whether checksums match
+                if (checksum == hdr.GetChecksum()):
+                    self.cbOnRxAvailable(\
+                        hdr, \
+                        self.bufRx[SerialLinkHeader.GetSize():msgSize])
+
+                    self.ClearLeadingBufferBytesAndRecalibrate(msgSize)
+                else:
+                    self.ClearLeadingBufferBytesAndRecalibrate(1)
+
+    def OnTimeoutCheckRx(self):
+        if self.state == "LOOKING_FOR_PREAMBLE_BYTE":
+            self.TryToSyncStreamFromSerial()
+
+        if self.state == "LOOKING_FOR_END_OF_MESSAGE":
+            self.TryToAddMoreDataFromSerial()
+
+            self.TryToProcessMessageFromCachedData()
 
         evm_SetTimeout(self.OnTimeoutCheckRx, self.RX_POLL_PERIOD_MS)
 
+
+
+class TestSerialLink():
+    def __init__(self, bcPinRx, bcPinTx):
+        self.SEND_INTERVAL_MS = 2000
+
+        # Get a SerialLink
+        self.serialLink = SerialLink()
+        self.serialLink.Init(bcPinRx, bcPinTx, self.OnRx)
+
+        # Set up timer to periodically send data
+        evm_SetTimeout(self.OnTimeoutTx, self.SEND_INTERVAL_MS)
+
+    def OnRx(self, hdr, byteList):
+        print("OnRx")
+
+        Log("HDR: %i bytes" % hdr.GetSize())
+        Log("  Preamble  : %3i" % hdr.GetPreamble())
+        Log("  DataLength: %3i" % hdr.GetDataLength())
+        Log("  Checksum  : %3i" % hdr.GetChecksum())
+        Log("  ProtocolId: %3i" % hdr.GetProtocolId())
+
+        count = len(byteList)
+        Log("MSG: %i bytes" % count)
+        for byte in byteList:
+            Log(("  %3i" % byte))
+
     def OnTimeoutTx(self):
-        self.Send("heynow")
-        evm_SetTimeout(self.OnTimeoutTx, 2000)
-
-
+        protocolId = 1
+        self.serialLink.Send(protocolId, "heynow")
+        evm_SetTimeout(self.OnTimeoutTx, self.SEND_INTERVAL_MS)
 
 
 def Main():
@@ -156,14 +286,10 @@ def Main():
     bcPinRx = int(sys.argv[1])
     bcPinTx = int(sys.argv[2])
 
-    serialLink = SerialLink()
-    serialLink.Init(bcPinRx, bcPinTx, None)
+    tsl = TestSerialLink(bcPinRx, bcPinTx)
 
     Log("Waiting")
     evm_MainLoop()
-
-#    pigd.bb_serial_read_close(bcPinRx)
-#    pigd.stop()
 
 
 Main()
