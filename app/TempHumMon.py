d144924e17f8bf2471a8e546b9ff597c379e674a
Douglas Malnati <douglas.malnati@gmail.com>
2018-02-14 22:47:14 -0500

    Pi TempHumMon initial checkin

---
+++ app/TempHumMon.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+
+
+def Main():
+    if len(sys.argv) != 4:
+        print("Usage: " + sys.argv[0] + " <arg>")
+        sys.exit(-1)
+
+    arg = sys.argv[1]
+
+
+
+Main()
+
+
+
+
+
