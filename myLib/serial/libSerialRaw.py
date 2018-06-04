2fcb6e406a4be7455277e80f85b5d8ba665d7d47
dmalnati <douglas.malnati@gmail.com>
2018-06-03 20:27:55 -0400

    Pi libSerialRaw enables glitch filter by default

--- myLib/serial/libSerialRaw.py
+++ myLib/serial/libSerialRaw.py
@@ -15,7 +15,7 @@ class SerialRaw():
                  bcPinRx=None,
                  bcPinTx=None,
                  baud=9600,
-                 pollPeriodRx=50):
+                 pollPeriodRx=1):
         self.BAUD              = baud
         self.RX_POLL_PERIOD_MS = pollPeriodRx
 
@@ -41,6 +41,14 @@ class SerialRaw():
     def SetCbOnRxAvailable(self, cbOnRxAvailable):
         self.cbOnRxAvailable = cbOnRxAvailable
 
+    def EnableGlitchFilter(self):
+        # Sets a glitch filter on a GPIO. 
+        # Level changes on the GPIO are not reported unless the level has been
+        # stable for at least steady microseconds.
+        # The level is then reported.
+        # Level changes of less than steady microseconds are ignored. 
+        self.pigd.set_glitch_filter(self.bcPinRx, 10)
+
     def InitRx(self):
         if self.bcPinRx:
             # open pin for reading serial
@@ -48,6 +56,9 @@ class SerialRaw():
             self.pigd.bb_serial_read_close(self.bcPinRx)
             pigpio.exceptions = True
             self.pigd.bb_serial_read_open(self.bcPinRx, self.BAUD)
+
+            # Set up glitch filter
+            self.EnableGlitchFilter()
 
             # set up timer to check on incoming data
             evm_SetTimeout(self.OnTimeoutCheckRx, self.RX_POLL_PERIOD_MS)
