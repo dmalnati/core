8286a0745b3f288f76839fed025f3b891dc1309d
Douglas Malnati <douglas.malnati@gmail.com>
2018-08-29 21:56:51 -0400

    Pi HomeControl add power strip, deal with inverted logic

--- rh1/HomeControl.py
+++ rh1/HomeControl.py
@@ -10,28 +10,49 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', ''))
 from myLib.app import *
 
 
+
+class Pin():
+    def __init__(self, highLowType, bcPin):
+        self.highLowType = highLowType
+        self.bcPin       = bcPin
+
+    def GetHighLow(self, logicalValue):
+        retVal = logicalValue
+
+        if self.highLowType == "LOW":
+            if logicalValue:
+                retVal = 0
+            else:
+                retVal = 1
+
+        return retVal
+
+    def GetPin(self):
+        return self.bcPin
+        
+
 class MyApp(WebApplicationBase):
     def Start(self):
         self.service__pin = {
             # Air Conditioners
-            "AC1" : 20,
-            "AC2" : 21,
+            "AC1" : Pin("HIGH", 20),
+            "AC2" : Pin("HIGH", 21),
             # Power Strip
-            "PS1" : 2,
-            "PS2" : 3,
-            "PS3" : 4,
-            "PS4" : 17,
-            "PS5" : 27,
-            "PS6" : 22,
+            "PS1" : Pin("LOW", 2),
+            "PS2" : Pin("LOW", 3),
+            "PS3" : Pin("LOW", 17),
+            "PS4" : Pin("LOW", 27),
+            "PS5" : Pin("LOW", 22),
+            "PS6" : Pin("LOW", 26),
         }
 
         self.pig = pigpio.pi()
 
         for service in sorted(self.service__pin.keys()):
             pin = self.service__pin[service]
-            Log("Setting " + service + "(pin " + str(pin) + ") to LOW")
-            self.pig.set_mode(pin, pigpio.OUTPUT)
-            self.pig.write(pin, 0)
+            Log("Setting " + service + "(pin " + str(pin.GetPin()) + ") to LOW")
+            self.pig.set_mode(pin.GetPin(), pigpio.OUTPUT)
+            self.pig.write(pin.GetPin(), pin.GetHighLow(0))
 
         Log("Starting")
 
@@ -41,22 +62,20 @@ class MyApp(WebApplicationBase):
     def OnInternetGet(self, name):
         retVal = 0
 
-        #Log("MyApp:OnInternetGet: " + name)
-
         if name in self.service__pin:
             pin = self.service__pin[name]
-            retVal = self.pig.read(pin)
+            retVal = pin.GetHighLow(self.pig.read(pin.GetPin()))
 
-        #Log("  returning: " + str(retVal))
+        #Log("MyApp:OnInternetGet: " + name + " => " + str(retVal))
 
         return str(retVal)
 
     def OnInternetSet(self, name, value):
-        Log("MyApp:OnInternetSet: " + name + " = " + str(value))
-
         if name in self.service__pin:
             pin = self.service__pin[name]
-            self.pig.write(pin, int(value))
+            self.pig.write(pin.GetPin(), pin.GetHighLow(int(value)))
+
+        Log("MyApp:OnInternetSet: " + name + " <= " + str(value))
 
 
 def Main():
